b3ab277b20b12a53b4d2cc7311bb5d7b062fdfc1
mukutdanielroy <mdanielr007@gmail.com>
2023-03-30 22:46:03 +0600

    Longest Palindromic Substring Update

--- python3/5. Longest Palindromic Substring.py
+++ python3/5. Longest Palindromic Substring.py
@@ -19,25 +19,12 @@ class Solution:
             return s[0]
         left, first_index = 0, 0
         right = last_index = len(s)-1
-        while right > left:
-            if s[left] == s[right]:
-                left += 1
-                right -=1
-                continue
-            if s[left] == s[right-1]:
-                last_index = right-1
-                right -=1
-                continue
-            if s[left+1] == s[right]:
-                first_index = left+1
-                left +=1
-                continue
-            if s[left+1] == s[right-1]:
-                first_index = left+1
-                last_index = right-1
-                right -= 1
-                left += 1
-                continue
+        for i, c in enumerate(s):
+            while right > left:
+                if s[i] == s[right]:
+                    left += 1
+                    right -= 1
+                    
         return s[first_index:last_index+1]
 
-print(Solution.longestPalindrome("eabcb"))
+print(Solution.longestPalindrome("eabcbdf"))
